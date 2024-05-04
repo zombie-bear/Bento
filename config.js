3ccbe5a426b93fba6d2c13be86536f1cde5c7f82
zombie-bear <75374561+zombie-bear@users.noreply.github.com>
2024-05-04 17:43:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'bcd0c43286334c7ca770a3b6691ed02e', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
